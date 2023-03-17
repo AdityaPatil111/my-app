0e630a9d8196c13b68ec1d86caf0b900d3b72bb6
Aditya <adityapatil8975@gmail.com>
2023-03-18 00:51:50 +0530

    commit
    Co-authored-by: omkarjadhav1123 <omkarjadhav1123@users.noreply.github.com>

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,5 @@
 import { Avatar, IconButton } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useState ,useEffect} from 'react'
 import './Chat.css'
 import SearchIcon from '@mui/icons-material/Search';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
@@ -7,18 +7,57 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 import MicIcon from '@mui/icons-material/Mic';
 import axios from './axios';
+import {useStateValue} from "./StateProvider";
+import { useParams } from 'react-router-dom';
+// import Pusher from 'pusher-js/types/src/core/pusher';
+import Pusher from "pusher-js";
 
-export default function Chat({messages}) {
+
+export default function Chat() {
 
    const [input,setInput]=useState("");
+   const [roomName,setRoomName]=useState("");
+   const [{user}]=useStateValue();
+   const [updatedAt, setUpdatedAt] = useState("");
+   const {roomId}=useParams();
+   const[messages, setMessages] = useState([]);
+
+   useEffect(()=>{
+      if(roomId){
+         axios.get(`http://localhost:8000/room/${roomId}`).then((response)=>{
+            setRoomName(response.data.name);
+            setUpdatedAt(response.data.updateAt);
+         });
+         axios.get(`http://localhost:8000/messages/${roomId}`).then((response)=>{
+            setMessages(response.data);
+         });
+         
+      }
+   }, [roomId]);
+
+   useEffect(()=>{
+      const pusher = new Pusher('6eeb781fc3b777771f72', {
+          cluster: 'ap2'
+        });
+        const  channel = pusher.subscribe('messages');
+        channel.bind("inserted", function(message) {
+          setMessages((prevMessages)=>[...prevMessages,message]);
+        });
+  },[]);
+
+
    const sendMessage = async(e)=>{
       e.preventDefault();
-
-      await axios.post("/api/v1/messages/new",{
+      if(!input){
+         return;
+      }
+      await axios.post("http://localhost:8000/api/v1/messages/chat",{
          message:input,
-         name:"Aditya",
-         timestamp:"just now",
-         received:true,
+         name:user.displayName,
+         timestamp:new Date(),
+         uid: user.uid,
+         roomId:roomId,
+         
       });
       setInput("");
    };
@@ -26,11 +65,14 @@ export default function Chat({messages}) {
   return (
      <div className='chat'>
         <div className='chat_header'>
-            <Avatar/>
+            <Avatar src={`https://api.dicebear.com/5.x/adventurer/svg`}/>
 
             <div className='chat_headerInfo'>
-                <h3>Room name</h3>
-                <p>Last seen at...</p>
+                <h3>{roomName?roomName:"Welcome to Whatsapp"}</h3>
+                <p>{updatedAt
+                ? `Last updated at ${new Date(updatedAt).toString().slice(0,25)}`
+                :"click on any group"
+                } {new Date().toString().slice(0,25)}</p>
             </div>
 
             <div className='chat_headerRight'>
@@ -48,44 +90,49 @@ export default function Chat({messages}) {
             </div>
         </div>
         <div className='chat_body'>
-         {messages.map((message)=>(
-             <p className={`chat_message ${message.received && "chat_reciever"}`}>
+         {messages.map((message,index)=>(
+             <p className={`chat_message ${message.uid === user.uid && "chat_reciever"}`}key={index}>
              <span className='chat_name'>{message.name}</span>
              {message.message}
-             <span className='chat_timestamp'>{message.timestamp}</span>
+             <span className='chat_timestamp'>{new Date(message.timestamp).toString().slice(0,25)}</span>
           </p>
 
          ))}
 
-</div>
+{/* </div> */} 
 
-
-{/*            
-
-            <p className='chat_message chat_reciever'>
-               <span className='chat_name'>Sonny</span>
-               This is message
-               <span className='chat_timestamp'>{new
-               Date().toUTCString()}</span>
-            </p>
-
-            <p className='chat_message'>
+            {/* <p className='chat_message'>
                <span className='chat_name'>Sonny</span>
                This is message
                <span className='chat_timestamp'>{new
                Date().toUTCString()}</span>
             </p> */}
-      
+            {/* <p className='chat_message chat_reciever'>
+               <span className='chat_name'>Sonny</span>
+               This is message
+               <span className='chat_timestamp'>{new
+               Date().toUTCString()}</span>
+            </p> */}
+      </div>
 
-        <div className='chat_footer'>
+       {roomName && < div className='chat_footer'>
                <InsertEmoticonIcon/>
                <form>
                   <input value={input} onChange={e=>setInput(e.target.value)}type="text" placeholder='Type a message'/>
                   <button onClick={sendMessage} type="submit">Send a message</button>
                </form>
                <MicIcon/>
-        </div>
+        </div>}
     </div>
 
   )
 }
+
+// export default function Chat(){
+//    return(
+//          <div>
+//             <h1>Chat</h1>
+
+//          </div>
+//    );
+// };
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect ,useState} from 'react'
 import './Sidebar.css'
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import { Avatar,IconButton} from '@mui/material';
@@ -6,13 +6,38 @@ import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import SearchIcon from '@mui/icons-material/Search';
 import SidebarChat from './SidebarChat';
+import { useStateValue } from './StateProvider';
+import axios from './axios';
+import Pusher from "pusher-js";
 
 export default function Sidebar() {
+
+    const [{user}]=useStateValue();
+    const [rooms,setRooms]=useState([]);
+
+    useEffect(()=>{
+        axios.get("http://localhost:8000/api/v1/messages/room").then((response)=>{
+            setRooms(response.data);
+        });
+    },[]);
+    console.log(rooms);
+
+    useEffect(()=>{
+        const pusher = new Pusher('6eeb781fc3b777771f72', {
+            cluster: 'ap2'
+          });
+          const  channel = pusher.subscribe('room');
+          channel.bind("inserted", function(room) {
+            setRooms((prevRooms)=>[...prevRooms,room]);
+          });
+    },[]);
+
+
   return (
    
     <div className='sidebar'>
         <div className='sidebar_header'>
-            <Avatar src='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80'/>
+            <Avatar src={user.photoURL}/>
             <div className='sidebar_headerRight'>
                 <IconButton>
                     <DonutLargeIcon/>
@@ -34,9 +59,11 @@ export default function Sidebar() {
             </div>
         </div>
         <div className='sidebar_chat'>
-            <SidebarChat/>
-            <SidebarChat/>
-            <SidebarChat/>
+            <SidebarChat addNewChat/>
+            {rooms.map((room) => (
+                <SidebarChat key={room._id} id={room._id} name={room.name}/>
+            ) )}
+            
         </div>
 
     </div>
